49c1f6a505fe7f8712056481d2ccb5c886996576
Antonio Dambujo <pdambuja@gmail.com>
2022-01-17 10:20:25 +0200

    add Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,78 @@
 import React from 'react';
+import styled from 'styled-components';
+import MenuIcon from '@material-ui/icons/Menu';
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <Container>
+      <a href="#">
+        <img scr="/images/logo-1.svg" alt="Tesla" />
+      </a>
+      <Menu>
+        <a>
+          <a href="#"> Model S</a>
+        </a>
+        <a>
+          <a href="#"> Model 3</a>
+        </a>
+        <a>
+          <a href="#"> Model X</a>
+        </a>
+        <a>
+          <a href="#"> Model Y</a>
+        </a>
+      </Menu>
+      <RightMenu>
+        <a href="#"> Shop</a>
+        <a href="#"> Tesla Account</a>
+        <CustomerMenu />
+      </RightMenu>
+    </Container>
+  );
 }
-
 export default Header;
+
+const Container = styled.div`
+  min-height: 60px;
+  position: fixed;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 20px;
+  top: 0;
+  left: 0;
+  right: 0;
+`;
+
+const Menu = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    padding: 0 10px;
+    flex-wrap: nowrap;
+  }
+
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const RightMenu = styled.div`
+  display: flex;
+  align-items: center;
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    padding: 0 10px;
+    flex-wrap: nowrap;
+  }
+`;
+
+const CustomerMenu = styled(MenuIcon)`
+  cursor: pointer;
+`;
